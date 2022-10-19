c30c92f7db310a9851f7b2887ee31f55739bed41
Bruno-Vitor <brunovitorc@gmail.com>
2022-10-19 00:27:08 -0300

    First Commit

---
+++ buscador/src/App.js
@@ -0,0 +1,30 @@
+import { FiSearch } from 'react-icons/fi';
+ 
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 23456-199</h2>
+        <span>Rua Teste</span>
+        <span>Complemento: Isso aí mesmo</span>
+        <span>Vila do Chaves</span>
+        <span>Tangamandápio</span>
+      </main>
+    </div>
+  );
+}
+
+export default App;
